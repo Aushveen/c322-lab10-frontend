9de8ec6bd6572b3c86b651ce164d65c24e4641bf
Aushveen Vimalathas <aushveen@gmail.com>
2024-04-24 17:08:18 -0400

    Final v

--- configuration.js
+++ configuration.js
@@ -7,4 +7,48 @@ function getHost() {
     return (mode == 0) ? host_local : host_remote;
 }
 
-let configuration = {loggedIn: false, hosts: getHost(), token: ""};
+function isLoggedIn() {
+    if (localStorage.getItem("token")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  function getTheToken() {
+    return localStorage.getItem("token");
+  }
+  
+  function saveTheToken(token) {
+    localStorage.setItem("token", token);
+    updateTheNavigationBar();
+  }
+  
+  function removeTheToken() {
+    localStorage.removeItem("token");
+    updateTheNavigationBar();
+  }
+  
+  let configuration = {
+    isLoggedIn: () => isLoggedIn(),
+    host: () => getHost(),
+    token: () => getTheToken(),
+  };
+  
+  updateTheNavigationBar();
+  
+  async function updateTheNavigationBar() {
+    const navigation = document.getElementsByClassName("topnav")[0];
+  
+    let loginTag = navigation.children[navigation.children.length - 1];
+    if (configuration.isLoggedIn()) {
+      loginTag.innerHTML = `<li class="right"><a href="#" onclick="logout()">Logout</a></li>`;
+    } else {
+      loginTag.innerHTML =
+        '<li class="right"><a href="login.html">Login</a></li>';
+    }
+  }
+
+  async function logout() {
+    removeTheToken();
+  }
--- login.js
+++ login.js
@@ -1,32 +1,32 @@
+async function login() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
 
+  let customer = { username: username, password: password };
+  let request = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(customer),
+  };
+  try {
+    let response = await fetch(getHost() + "/signin", request);
+    if (response.status == 200) {
+      alert("The login was successful!");
+      const token = await response.text();
+      saveTheToken(token);
+      location.href = "index.html";
+    } else {
+      location.href = "index.html";
 
-async function login() {    
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    let customer = {username: username, password: password}
-    let request = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(customer)
-      };
-      try {
-        let response = await fetch("https://lab10-latest-2e31.onrender.com" + "/signin", request);
-        if(response.status == 200) {  
-            alert("The login was successful!");
-            const token = await response.text();
-            configuration.token = token;
-            configuration.loggedIn = true;
-            location.href = "index.html";
-
-        } else {
-            console.log(`response status:${response.status}`);            
-            alert("Something went wrong!");
-        }
-      }
-      catch(error) {
-        console.log(error);        
-        alert("Something went wrong!");
-      }    
+      console.log("response status: ${response.status}");
+      removeTheToken();
+      alert("Something went wrong!");
+    }
+  } catch (error) {
+    console.log(error);
+    removeTheToken();
+    alert("Something went wrong!");
+  }
 }
